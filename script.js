9115216aa93852123c03921048072486a646fd4d
Steve Simonson <srsimonson@gmail.com>
2021-01-29 17:09:39 -0600

    branch integrated

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var linkData = {
-    campaign: 'TEST FROM SCRIPT',
-    channel: 'facebook',
-    feature: 'dashboard',
-    stage: 'new user',
-    tags: [ 'tag1', 'tag2', 'tag3' ],
-    alias: '',
-    data: {
-        'custom_bool': true,
-        'custom_int': Date.now(),
-        'custom_string': 'hello',
-        '$og_title': 'Title',
-        '$og_description': 'Description',
-        '$og_image_url':'http://lorempixel.com/400/400'
-    }
-};
+// var linkData = {
+//     campaign: 'TEST FROM SCRIPT',
+//     channel: 'facebook',
+//     feature: 'dashboard',
+//     stage: 'new user',
+//     tags: [ 'tag1', 'tag2', 'tag3' ],
+//     alias: '',
+//     data: {
+//         'custom_bool': true,
+//         'custom_int': Date.now(),
+//         'custom_string': 'hello',
+//         '$og_title': 'Title',
+//         '$og_description': 'Description',
+//         '$og_image_url':'http://lorempixel.com/400/400'
+//     }
+// };
 
 // const branchEvent = () => {
 //     console.log('hi');
