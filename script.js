d4fc378184f162245803619324c9694ba7d091bf
Steve Simonson <srsimonson@gmail.com>
2021-01-29 17:08:20 -0600

    branch integrated

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const branchEvent = () => {
-    console.log('hi');
-    window.open('http://frankschili.app.link/KrYNWMS0rdb');
-}
+var linkData = {
+    campaign: 'TEST FROM SCRIPT',
+    channel: 'facebook',
+    feature: 'dashboard',
+    stage: 'new user',
+    tags: [ 'tag1', 'tag2', 'tag3' ],
+    alias: '',
+    data: {
+        'custom_bool': true,
+        'custom_int': Date.now(),
+        'custom_string': 'hello',
+        '$og_title': 'Title',
+        '$og_description': 'Description',
+        '$og_image_url':'http://lorempixel.com/400/400'
+    }
+};
+
+// const branchEvent = () => {
+//     console.log('hi');
+//     window.open('http://frankschili.app.link/KrYNWMS0rdb');
+// }
