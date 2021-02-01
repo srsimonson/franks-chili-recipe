5eb58ac0b95739f333168234de493b7339772cc5
Steve Simonson <srsimonson@gmail.com>
2021-02-01 10:45:02 -0600

    cleaned up

--- script.js
+++ script.js
@@ -1,38 +1,23 @@
-// var linkData = {
-//     campaign: 'TEST FROM SCRIPT',
-//     channel: 'facebook',
-//     feature: 'dashboard',
-//     stage: 'new user',
-//     tags: [ 'tag1', 'tag2', 'tag3' ],
-//     alias: '',
-//     data: {
-//         'custom_bool': true,
-//         'custom_int': Date.now(),
-//         'custom_string': 'hello',
-//         '$og_title': 'Title',
-//         '$og_description': 'Description',
-//         '$og_image_url':'http://lorempixel.com/400/400'
-//     }
-// };
-
-// const branchEvent = () => {
-//     console.log('hi');
-//     window.open('http://frankschili.app.link/KrYNWMS0rdb');
-// }
-
-var wizards = ['Hermione', 'Neville', 'Gandalf'];
-
-// Get the app element
-var app = document.querySelector('#app');
-
-// Create markup
-app.innerHTML = '<ul>' + wizards.map(function (wizard) {
-	return '<li>' + wizard + '</li>';
-}).join('') + '</ul>';
-
-const hiThere = document.querySelector
-
-const myElements = [1, 2, 3]
-Array.from(myElements).forEach('<ul>' + myElements.map(function (wizard) {
-	return '<li>' + wizard + '</li>';
-}).join('') + '</ul>')
+var linkData = {
+	campaign: 'Range Digital Test',
+	channel: 'facebook',
+	feature: 'dashboard',
+	stage: 'new user',
+	tags: [ 'tag1', 'tag2', 'tag3' ],
+	alias: '',
+	data: {
+	  'custom_bool': true,
+	  'custom_int': Date.now(),
+	  'custom_string': 'hello',
+	  '$og_title': 'Title',
+	  '$og_description': 'Description',
+	  '$og_image_url':'http://lorempixel.com/400/400'
+	}
+  };
+  
+  branch.link(linkData, function(err, link) {
+	document.getElementById('button').onclick = function() {
+	  window.open(link || err);
+	};
+  //   document.getElementById('anchor').href = link || err;
+  });
