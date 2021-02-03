fac3d46ac14a36cf4f048c11372eadd0f3cedd31
Steve Simonson <srsimonson@gmail.com>
2021-02-03 12:53:10 -0600

    commit

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-// var linkData = {
-// 	campaign: 'Range Digital Test',
-// 	channel: 'facebook',
-// 	feature: 'dashboard',
-// 	stage: 'new user',
-// 	tags: [ 'tag1', 'tag2', 'tag3' ],
-// 	alias: '',
-// 	data: {
-// 	  'custom_bool': true,
-// 	  'custom_int': Date.now(),
-// 	  'custom_string': 'hello',
-// 	  '$og_title': 'Title',
-// 	  '$og_description': 'Description',
-// 	  '$og_image_url':'http://lorempixel.com/400/400'
-// 	}
-//   };
+var linkData = {
+	campaign: 'Range Digital Test',
+	channel: 'facebook',
+	feature: 'dashboard',
+	stage: 'new user',
+	tags: [ 'tag1', 'tag2', 'tag3' ],
+	alias: '',
+	data: {
+	  'custom_bool': true,
+	  'custom_int': Date.now(),
+	  'custom_string': 'hello',
+	  '$og_title': 'Title',
+	  '$og_description': 'Description',
+	  '$og_image_url':'http://lorempixel.com/400/400'
+	}
+  };
   
-//   branch.link(linkData, function(err, link) {
-// 	document.getElementById('button').onclick = function() {
-// 	  window.open(link || err);
-// 	};
-//   //   document.getElementById('anchor').href = link || err;
-//   });
+  branch.link(linkData, function(err, link) {
+	document.getElementById('button').onclick = function() {
+	  window.open(link || err);
+	};
+  //   document.getElementById('anchor').href = link || err;
+  });
